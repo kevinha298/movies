c6397e45c24921f7fbf3b767634543c752259d0b
Kevin Ha <kevinha298@live.com>
2020-03-29 21:34:43 -0700

    Initial project commit.

---
+++ src/api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from api.models import Movie
+from api.serializers import MovieSerializer
+
+
+class MovieCreateView(ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class MovieDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
